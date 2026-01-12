58a2f8fe5e352c848d6c35ab1d1395a6ab923c96
EstherLaw404 <xuexue0404@gmail.com>
2026-01-12 14:25:43 +0800

    backend api

---
+++ backend/index.php
@@ -0,0 +1,25 @@
+<?php
+header('Content-Type: application/json');
+
+// Get number of people from query parameter
+$numPeople = isset($_GET['people']) ? intval($_GET['people']) : 0;
+
+// Create deck
+$suits = ["S","H","D","C"];
+$values = ["A","2","3","4","5","6","7","8","9","X","J","Q","K"];
+$deck = [];
+foreach ($suits as $suit) {
+    foreach ($values as $value) {
+        $deck[] = "$suit-$value";
+    }
+}
+shuffle($deck);
+
+// Distribute cards
+$distributed = [];
+for ($i = 0; $i < $numPeople; $i++) $distributed[$i] = [];
+foreach ($deck as $index => $card) {
+    $distributed[$index % $numPeople][] = $card;
+}
+
+echo json_encode($distributed);
